76778c312a91077c32bc54ea016d114145e551b8
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2020-01-26 06:49:03 +0000

    Added router

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import React  from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import App from './App';
+import App from './components/home_page/App';
 
 export default function Router(){
     return(
